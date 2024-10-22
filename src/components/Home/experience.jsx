5b02ea72aaf954d188817dfd9d633ba6ec2aed22
allenki_akshay <allenkiakshay8322@gmail.com>
2024-10-23 01:06:44 +0530

    Refactor experience card component: Update layout and styling

--- src/components/Home/experience.jsx
+++ src/components/Home/experience.jsx
@@ -3,40 +3,51 @@ import ExperienceCard from "./cards/experienceCard";
 const Experience = ({ count }) => {
   const experienceList = [
     {
-      title: "Software Developer Intern",
-      company: "Tech Solutions",
-      duration: "May 2024 - Aug 2024",
+      title: "Machine Learning Intern",
+      company: "Indian Institute of Technology, Bombay",
+      duration: "May 2023 - May 2024",
       description:
-        "Worked on developing web applications using React.js and Node.js. Contributed to improving the performance of the app and optimizing the database queries.",
-      techStack: "React.js, Node.js, MongoDB",
-      companyLogo: "https://yourimageurl.com/tech-solutions-logo.jpg",
-    },
-    {
-      title: "Frontend Developer",
-      company: "WebDev Co.",
-      duration: "Jan 2023 - Apr 2023",
-      description:
-        "Developed and maintained the frontend of the company's e-commerce platform, ensuring responsive design and smooth user experience.",
-      techStack: "Next.js, Tailwind CSS, Stripe",
-      companyLogo: "https://yourimageurl.com/webdev-logo.jpg",
+        "Worked on automated speech recognition using deep learning techniques and Neural Networks. Developed an application that integrates the model to convert speech to text.",
+      techStack:
+        "React.js, Python3, SQLite, AWS, WhisperX, Transformers, PyTorch, pydub",
     },
     {
       title: "Software Developer Intern",
-      company: "Tech Solutions",
-      duration: "May 2024 - Aug 2024",
+      company: "VIT-AP University",
+      duration: "SEP 2023 - NA",
       description:
-        "Worked on developing web applications using React.js and Node.js. Contributed to improving the performance of the app and optimizing the database queries.",
-      techStack: "React.js, Node.js, MongoDB",
-      companyLogo: "https://yourimageurl.com/tech-solutions-logo.jpg",
+        "Developed Official Website of VIT-AP University. Worked on developing web applications using Latest Technologies. Contributed to improving the performance of the app and optimizing the database queries.Deployed the website on AWS.",
+      // techStack: "Next.js, Tailwind CSS, Stripe",
     },
     {
-      title: "Frontend Developer",
-      company: "WebDev Co.",
-      duration: "Jan 2023 - Apr 2023",
+      title: "Software Developer Intern",
+      company: "TimeseraErp Software Solutions",
+      duration: "May 2024 - NA",
       description:
-        "Developed and maintained the frontend of the company's e-commerce platform, ensuring responsive design and smooth user experience.",
-      techStack: "Next.js, Tailwind CSS, Stripe",
-      companyLogo: "https://yourimageurl.com/webdev-logo.jpg",
+        "Worked on Backend Development of the company's Products. Developed and maintained the backend of the company's software platforms, ensuring responsive design and smooth user experience.Worked on Devops and CI/CD pipelines.",
+      techStack: "Next.js, Angular, MS SQL, Microsoft Azure",
+    },
+    {
+      title: "Software Developer Intern",
+      company: "Navigate Labs AI",
+      duration: "SEP 2024 - NA",
+      description:
+        "Developing a AI based code editor that can help developers to write code faster and more efficiently. Worked on developing web applications using Latest Technologies.",
+      // techStack: "Next.js, Tailwind CSS, Stripe",
+    },
+    {
+      title: "Web Development Team Lead",
+      company: "MLC Chapter VIT-AP",
+      duration: "SEP 2024 - NA",
+      description:
+        "Led a team of 10 members to develop a web application for the college fest. Conducted workshops on Web Development and Competitive Programming. Organized Hackathons and Coding Competitions.",
+    },
+    {
+      title: "Technical Team Lead",
+      company: "CSI Chapter VIT-AP",
+      duration: "MAY 2023 - AUG 2024",
+      description:
+        "Led a team of 10 members to develop a web application for the college fest. Conducted workshops on Web Development and Competitive Programming. Organized Hackathons and Coding Competitions.",
     },
   ];
 
@@ -55,7 +66,6 @@ const Experience = ({ count }) => {
           duration={experience.duration}
           description={experience.description}
           techStack={experience.techStack}
-          companyLogo={experience.companyLogo}
         />
       ))}
 
